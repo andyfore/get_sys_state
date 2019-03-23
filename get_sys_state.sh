40c2d317970854f67ffc36dc6a4419a4234cd663
Andy Fore <andyfore@users.noreply.github.com>
2019-03-23 01:05:25 -0400

    Update get_sys_state.sh
    1. Updated the various usages of the "tee" command to suppress the stdout by use of /dev/null
    2. Updated multiple cases to trap for existence of config files to prevent "error" lines from being printed

--- get_sys_state.sh
+++ get_sys_state.sh
@@ -12,7 +12,7 @@ output_file="/opt/system_state_`date +%Y%m%d`"
 
 ## Non-Static Variables
 # Variable name: os_type
-# Purposes: holds the type of OS on the system for use in 
+# Purposes: holds the type of OS on the system for use in
 #           determining what commands to run
 os_type=""
 
@@ -33,7 +33,7 @@ echo "## Basic System Info" >> $output_file
 echo "############################################" >> $output_file
 echo "Hostname: `hostname`" >> $output_file
 case $os_type in
-	Linux) echo "Running: $os_type" >> $output_file
+        Linux) echo "Running: $os_type" >> $output_file
            if [ -f /etc/redhat-release ]
            then
                echo "RedHat-Based Distro: `cat /etc/redhat-release`" >> $output_file
@@ -58,7 +58,7 @@ case $os_type in
            echo "Processor type: `uname -p`" >> $output_file
            echo "Version: `uname -r`" >> $output_file
            if [ `uname -r` == "5.11" ]; then
-                pkg info kernel | tee -a $output_file
+                pkg info kernel | tee -a $output_file 1> /dev/null
            else
                 echo "Kernel: `uname -v`" >> $output_file
            fi
@@ -93,7 +93,7 @@ echo "" >> $output_file
 
 # Filesystem Information
 case $os_type in
-	Linux)   echo "############################################" >> $output_file
+        Linux)   echo "############################################" >> $output_file
            echo "## /etc/fstab" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
@@ -122,38 +122,40 @@ echo "## Mounted Filesystems" >> $output_file
 echo "############################################" >> $output_file
 echo "" >> $output_file
 echo "" >> $output_file
-mount | tee -a $output_file
+mount | tee -a $output_file 1> /dev/null
 echo "" >> $output_file
 echo "" >> $output_file
 # Filesystem Usage
 case $os_type in
-	Linux)   echo "############################################" >> $output_file
+        Linux)   echo "############################################" >> $output_file
            echo "## Filesystem Usage" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           df -hT | tee -a $output_file
+           df -hT | tee -a $output_file 1> /dev/null
            ;;
     AIX)   echo "############################################" >> $output_file
            echo "## Filesystem Usage" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           df -Pg | tee -a $output_file
+           df -Pg | tee -a $output_file 1> /dev/null
            ;;
     SunOS) echo "############################################" >> $output_file
            echo "## Filesystem Usage" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           df -h | tee -a $output_file
+           df -h | tee -a $output_file 1> /dev/null
            ;;
 esac
 echo "" >> $output_file
 echo "" >> $output_file
+
 # Firewall Info
 case $os_type in
-	Linux)   echo "############################################" >> $output_file
+        Linux)
+           echo "############################################" >> $output_file
            echo "## Firewall Service State" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
@@ -179,16 +181,18 @@ esac
 echo "" >> $output_file
 echo "" >> $output_file
 case $os_type in
-	Linux)   echo "" >> $output_file
+        Linux)
+           echo "" >> $output_file
            echo "" >> $output_file
            echo "############################################" >> $output_file
            echo "## running iptables configuration" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           /sbin/iptables -L -v -n | tee -a $output_file
+           /sbin/iptables -L -v -n | tee -a $output_file 1> /dev/null
            echo "" >> $output_file
            echo "" >> $output_file
+if [ -f /etc/sysconfig/iptables-config ]; then
            echo "############################################" >> $output_file
            echo "## iptables config file" >> $output_file
            echo "############################################" >> $output_file
@@ -197,19 +201,22 @@ case $os_type in
            cat /etc/sysconfig/iptables-config >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
+fi
+if [ -f /etc/sysconfig/iptables ]; then
            echo "############################################" >> $output_file
            echo "## iptables file" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
            cat /etc/sysconfig/iptables >> $output_file
+fi
            ;;
     AIX)   echo "############################################" >> $output_file
            echo "## Firewall Configuration" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           lsfilt | tee -a $output_file
+           lsfilt | tee -a $output_file 1> /dev/null
            ;;
     SunOS) echo "############################################" >> $output_file
            echo "## Firewall Configuration" >> $output_file
@@ -223,7 +230,8 @@ echo "" >> $output_file
 echo "" >> $output_file
 # Network Configuration
 case $os_type in
-	Linux)   echo "############################################" >> $output_file
+        Linux)
+           echo "############################################" >> $output_file
            echo "## Generic Network Info" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
@@ -256,16 +264,16 @@ echo "## ip address info" >> $output_file
 echo "############################################" >> $output_file
 echo "" >> $output_file
 echo "" >> $output_file
-ifconfig -a | tee -a $output_file
+ifconfig -a | tee -a $output_file 1> /dev/null
 echo "" >> $output_file
 echo "" >> $output_file
 case $os_type in
-	Linux)   echo "############################################" >> $output_file
+        Linux)   echo "############################################" >> $output_file
            echo "## Routing Table" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           ip ro | tee -a $output_file
+           ip ro | tee -a $output_file 1> /dev/null
            ;;
     AIX)   echo "############################################" >> $output_file
            echo "## Routing Table" >> $output_file
@@ -285,12 +293,13 @@ esac
 echo "" >> $output_file
 echo "" >> $output_file
 case $os_type in
-	Linux)   echo "############################################" >> $output_file
+    Linux)
+           echo "############################################" >> $output_file
            echo "## Listening Ports" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           netstat -ntlp | tee -a $output_file
+           netstat -ntlp | tee -a $output_file 1> /dev/null
            ;;
     AIX)   echo "############################################" >> $output_file
            echo "## Listening Ports" >> $output_file
@@ -310,7 +319,8 @@ esac
 echo "" >> $output_file
 echo "" >> $output_file
 case $os_type in
-	Linux) echo "############################################" >> $output_file
+    Linux)
+           echo "############################################" >> $output_file
            echo "## Network Interface Configuration" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
@@ -322,6 +332,7 @@ case $os_type in
                echo "" >> $output_file
                echo "" >> $output_file
            done
+if [ -f /etc/sysconfig/network-scripts/route-* ]; then
            echo "############################################" >> $output_file
            echo "## Routing Configuration Files" >> $output_file
            echo "############################################" >> $output_file
@@ -334,6 +345,7 @@ case $os_type in
                echo "" >> $output_file
                echo "" >> $output_file
            done
+fi
            echo "" >> $output_file
            echo "" >> $output_file
            ;;
@@ -366,12 +378,12 @@ case $os_type in
 esac
 
 case $os_type in
-	Linux)   echo "############################################" >> $output_file
+  Linux)   echo "############################################" >> $output_file
            echo "## installed package listing" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           rpm -qa | sort | tee -a $output_file
+           rpm -qa | sort | tee -a $output_file 1> /dev/null
            ;;
     AIX)   echo "############################################" >> $output_file
            echo "## installed package listing" >> $output_file
@@ -393,7 +405,7 @@ echo "" >> $output_file
 
 # Get yum config
 case $os_type in
-	Linux) echo "############################################" >> $output_file
+        Linux) echo "############################################" >> $output_file
            echo "## YUM config file - /etc/yum.conf" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
@@ -418,7 +430,9 @@ esac
 
 # Get the crontabs for all users
 case $os_type in
-	Linux)   echo "############################################" >> $output_file
+        Linux)
+if [ -f /var/spool/cron/* ]; then
+           echo "############################################" >> $output_file
            echo "## user crontabs" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
@@ -430,6 +444,7 @@ case $os_type in
                echo "" >> $output_file
                echo "" >> $output_file
            done
+fi
            ;;
     AIX)   echo "############################################" >> $output_file
            echo "## user crontabs" >> $output_file
@@ -460,7 +475,8 @@ case $os_type in
 esac
 
 case $os_type in
-	Linux) # Get the MPIO config
+        Linux) # Get the MPIO config
+if [ -f /etc/multipath.conf ]; then
            echo "############################################" >> $output_file
            echo "## /etc/multipath.conf" >> $output_file
            echo "############################################" >> $output_file
@@ -469,7 +485,7 @@ case $os_type in
            cat /etc/multipath.conf >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           
+
            # Get the multipath udev rules config
            echo "############################################" >> $output_file
            echo "## /etc/udev/rules.d/*-multipath.rules" >> $output_file
@@ -478,17 +494,18 @@ case $os_type in
            echo "" >> $output_file
            ls -l /etc/udev/rules.d/*-multipath.rules >> $output_file
            cat /etc/udev/rules.d/*-multipath.rules >> $output_file
+fi
            ;;
 esac
 
 case $os_type in
-	AIX)   # Get the SAN FC apadter information
+        AIX)   # Get the SAN FC apadter information
            echo "############################################" >> $output_file
            echo "## FC Adapter Info" >> $output_file
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           sanlun fcp show adapter -v all | tee -a $output_file
+           sanlun fcp show adapter -v all | tee -a $output_file 1> /dev/null
            echo "" >> $output_file
            echo "" >> $output_file
 
@@ -498,7 +515,7 @@ case $os_type in
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           powermt display dev=all | tee -a $output_file
+           powermt display dev=all | tee -a $output_file 1> /dev/null
            echo "" >> $output_file
            echo "" >> $output_file
 
@@ -508,7 +525,7 @@ case $os_type in
            echo "############################################" >> $output_file
            echo "" >> $output_file
            echo "" >> $output_file
-           lsdev -Ccdisk | tee -a $output_file
+           lsdev -Ccdisk | tee -a $output_file 1> /dev/null
            echo "" >> $output_file
            echo "" >> $output_file
            # Get the MPIO rules
@@ -534,26 +551,26 @@ case $os_type in
            ;;
 esac
 case $os_type in
-	SunOS)  # Get the zone information
-		echo "############################################" >> $output_file
-		echo "## Zone List" >> $output_file
-		echo "############################################" >> $output_file
-		echo "" >> $output_file
-		echo "" >> $output_file
-		/usr/sbin/zoneadm list -vi >> $output_file
+        SunOS)  # Get the zone information
+                echo "############################################" >> $output_file
+                echo "## Zone List" >> $output_file
+                echo "############################################" >> $output_file
+                echo "" >> $output_file
+                echo "" >> $output_file
+                /usr/sbin/zoneadm list -vi >> $output_file
     echo "" >> $output_file
     echo "" >> $output_file
-		;;
+                ;;
 esac
 
 case $os_type in
-	SunOS) # Get the zfs information
+        SunOS) # Get the zfs information
     echo "############################################" >> $output_file
     echo "## Zpool List" >> $output_file
     echo "############################################" >> $output_file
-		echo "" >> $output_file
-		echo "" >> $output_file
-		zpool list
+                echo "" >> $output_file
+                echo "" >> $output_file
+                zpool list
     echo "" >> $output_file
     echo "" >> $output_file
     echo "############################################" >> $output_file
@@ -561,14 +578,14 @@ case $os_type in
     echo "############################################" >> $output_file
     echo "" >> $output_file
     echo "" >> $output_file
-		zpool status
+                zpool status
     echo "" >> $output_file
     echo "" >> $output_file
-		;;
+                ;;
 esac
 
 case $os_type in
-	SunOS) # Get the iscsi information
+        SunOS) # Get the iscsi information
     echo "############################################" >> $output_file
     echo "## iSCSI initiator-node" >> $output_file
     echo "############################################" >> $output_file
@@ -601,5 +618,5 @@ case $os_type in
     iscsiadm list target -vS
     echo "" >> $output_file
     echo "" >> $output_file
-		;;
+                ;;
 esac
